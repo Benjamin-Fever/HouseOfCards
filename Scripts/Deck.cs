1fcec4fcc7f196ff60a549bf67f109b746231354
robbinasto <robbinasto@ecs.vuw.ac.nz>
2024-07-20 11:46:44 +1200

    struct

---
+++ Scripts/Deck.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using System.Collections;
+
+public partial class Deck : Node2D
+{
+	[Export] public int deckSize;
+	[Export] public int jokerCount;
+	[Export] public int pictureCardCount;
+	// Called when the node enters the scene tree for the first time.
+	ArrayList cards = new ArrayList();
+	public override void _Ready()
+	{
+		for(int i = 0; i < deckSize; i++){
+			if(jokerCount > 0){
+				
+			}
+
+		}
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+}
+
+struct cardData {
+	public enum suit { Spades, Hearts, Diamonds, Clubs };
+	public int value;
+	public suit cardSuit;
+
+}
