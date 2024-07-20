0857c5097c7934cfd1691bee1777ef71dd1ede3f
robbinasto <robbinasto@ecs.vuw.ac.nz>
2024-07-20 19:57:43 +1200

    deckreveal

--- Scripts/deckReveal.cs
+++ Scripts/deckReveal.cs
@@ -10,66 +10,53 @@ public partial class deckReveal : Control
 	[Export] private Texture2D _clubs;
 
 	private TextureRect cardShown;
-
-
-	public CardData cardData;
+	private MarginContainer margin => GetNode<MarginContainer>("MarginContainer");
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
-		int rows = Deck.singleton.cards.Count /5;
-		VBoxContainer vContainer = new VBoxContainer()
-		{
-			SizeFlagsHorizontal = SizeFlags.ExpandFill,
-            SizeFlagsVertical = SizeFlags.ExpandFill
-		};
-		GetNode("MarginContainer").AddChild(vContainer);
-		
-		for(int i = 0; i < rows; i++){
-            HBoxContainer container = new HBoxContainer
-            {
-                SizeFlagsHorizontal = SizeFlags.ExpandFill,
-                SizeFlagsVertical = SizeFlags.ExpandFill
-            };
-            for (int j = 0; j < 5; j++){
-                cardShown = new TextureRect
-                {
-                    ExpandMode = TextureRect.ExpandModeEnum.IgnoreSize,
-                    SizeFlagsHorizontal = SizeFlags.ExpandFill,
-                    SizeFlagsVertical = SizeFlags.ExpandFill
-                };
-				cardData = Deck.singleton.cards[i+j];
-				flipCard();
-                container.AddChild(cardShown);
+		int size = (int)Mathf.Sqrt(Deck.singleton.cards.Count);
+		HBoxContainer currentHbox = new HBoxContainer(){ SizeFlagsHorizontal = SizeFlags.ExpandFill, SizeFlagsVertical = SizeFlags.ExpandFill };
+		margin.AddChild(currentHbox);
+		for (int row = 0; row < size; row++){
+			currentHbox.AddChild(new VBoxContainer(){ SizeFlagsHorizontal = SizeFlags.ExpandFill, SizeFlagsVertical = SizeFlags.ExpandFill });
+			for (int col = 0; col < size; col++){
+				CardData cardData = Deck.singleton.cards[col * size + row];
+				TextureRect card = new TextureRect(){
+					SizeFlagsHorizontal = SizeFlags.ExpandFill,
+					SizeFlagsVertical = SizeFlags.ExpandFill,
+					Texture = getTexture(cardData)
+				};
+
+				currentHbox.GetChild(0).AddChild(card);
 			}
-			vContainer.AddChild(container);
-		}
-	}
-	public void flipCard(){
-		switch (cardData.cardSuit){
-			case CardData.suit.Spades:
-				setFrontTexture(_spades);
-				break;
-			case CardData.suit.Hearts:
-				setFrontTexture(_hearts);
-				break;
-			case CardData.suit.Diamonds:
-				setFrontTexture(_diamonds);
-				break;
-			case CardData.suit.Clubs:
-				setFrontTexture(_clubs);
-				break;
-		
 		}
 	}
 
-	private void setFrontTexture(Texture2D cardTexture){
+
+	private AtlasTexture getTexture(CardData cardData){
+		Texture2D texture2D = new Texture2D();
+		switch (cardData.cardSuit){
+			case CardData.suit.Spades:
+				texture2D = _spades;
+				break;
+			case CardData.suit.Hearts:
+				texture2D = _hearts;
+				break;
+			case CardData.suit.Diamonds:
+				texture2D = _diamonds;
+				break;
+			case CardData.suit.Clubs:
+				texture2D = _clubs;
+				break;
+		}
+
 		int y = Mathf.CeilToInt(cardData.value / 5);
 		int x = cardData.value % 5;
 		AtlasTexture singleCardTexture = new AtlasTexture(){
-			Atlas = cardTexture,
+			Atlas = texture2D,
 			Region = new Rect2(190 * x, 270 * y, 190, 270)
 		};
-		cardShown.Texture = singleCardTexture;
+		return singleCardTexture;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
