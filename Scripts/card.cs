5dc68afebf3c8e15193d1989cac3705d7ef59905
Benjamin Fever <ben.fever07@gmail.com>
2024-07-20 12:11:48 +1200

    added card stuff

--- Scripts/card.cs
+++ Scripts/card.cs
@@ -1,19 +1,46 @@
 using Godot;
 using System;
 
-public partial class card : Node2D
-{
-	public enum suit { Spades, Hearts, Diamonds, Clubs };
-	public int value;
-	public suit cardSuit;
+public partial class card : Node2D {
+	[ExportGroup("Card Textures")]
+	[Export] Texture2D spades;
+	[Export] Texture2D hearts;
+	[Export] Texture2D diamonds;
+	[Export] Texture2D clubs;
+	[ExportGroup("Refrences")]
+	[Export] Sprite2D cardFront;
 
+	public cardData cardInfo;
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
+		switch (cardInfo.cardSuit){
+			case cardData.suit.Spades:
+				cardFront.Texture = spades;
+				break;
+			case cardData.suit.Hearts:
+				cardFront.Texture = hearts;
+				break;
+			case cardData.suit.Diamonds:
+				cardFront.Texture = diamonds;
+				break;
+			case cardData.suit.Clubs:
+				cardFront.Texture = clubs;
+				break;
+		
+		}
+	}
+
+	private void setFrontTexture(Texture2D cardTexture){
+		int y = Mathf.CeilToInt(cardInfo.value / 5);
+		int x = cardInfo.value % 5;
+		AtlasTexture singleCardTexture = new AtlasTexture(){
+			Atlas = cardTexture,
+			Region = new Rect2(88 * x, 124 * y, 88, 124)
+		};
+		cardFront.Texture = singleCardTexture;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
 	}
 }
