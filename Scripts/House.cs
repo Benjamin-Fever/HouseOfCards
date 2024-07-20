62711034581f6f38ecbde97c932fc7600231c774
robbinasto <robbinasto@ecs.vuw.ac.nz>
2024-07-20 15:19:31 +1200

    added house of cards logic

--- Scripts/House.cs
+++ Scripts/House.cs
@@ -4,13 +4,26 @@ using System;
 public partial class House : Node2D
 {
 	[Export] private Sprite2D _house;
+	[Export] private Texture2D _houseT;
+	[Export] public int maxCards = 9;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		setTexture();
+		setTexture(Main.CardCounter);
 	}
 
-	private void setTexture(){
+	private void setTexture(int cardCount){
+		int y = 360;
+		int x = 390;
+		if(cardCount < maxCards){
+			y = 360/maxCards * cardCount;
+			x = 390/maxCards * cardCount;
+		}
+		AtlasTexture houseTexture = new AtlasTexture(){
+			Atlas = _houseT,
+			Region = new Rect2(0,360 - y,x,y)
+		};
+		_house.Texture = houseTexture;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
