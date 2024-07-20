6d6bc398379b341152d68b68adbc177b358bc170
robbinasto <robbinasto@ecs.vuw.ac.nz>
2024-07-20 12:10:31 +1200

    deck code

--- Scripts/Deck.cs
+++ Scripts/Deck.cs
@@ -4,33 +4,75 @@ using System.Collections;
 
 public partial class Deck : Node2D
 {
-	[Export] public int deckSize;
-	[Export] public int jokerCount;
-	[Export] public int pictureCardCount;
-	// Called when the node enters the scene tree for the first time.
-	ArrayList cards = new ArrayList();
-	public override void _Ready()
-	{
-		for(int i = 0; i < deckSize; i++){
-			if(jokerCount > 0){
-					
-			}
-			else if(pictureCardCount > 0){
+    [Export] public int deckSize;
+    [Export] public int jokerCount;
+    [Export] public int pictureCardCount;
+    // Called when the node enters the scene tree for the first time.
+    ArrayList cards = new ArrayList();
+    public override void _Ready()
+    {
+        int cardNum;
 
-			}
-		}
+        for (int i = 0; i < deckSize; i++)
+        {
+            cardData card;
+            if (jokerCount > 0)
+            {
+                cardNum = 14;
+                jokerCount--;
+            }
+            else if (pictureCardCount > 0)
+            {
+                int caseNum = (int)GD.RandRange(1, 5);
+                switch (caseNum)
+                {
+                    case 1:
+                        cardNum = 10;
+                        break;
+                    case 2:
+                        cardNum = 11; // Jack
+                        break;
+                    case 3:
+                        cardNum = 12; // Queen
+                        break;
+                    case 4:
+                        cardNum = 13; // King
+                        break;
+                    case 5:
+                        cardNum = 1; // Ace
+                        break;
+                    default:
+                        cardNum = 2; 
+                        break;
+                }
+                pictureCardCount--;
+            }
+            else
+            {
+                cardNum = (int)GD.RandRange(2, 9); 
+            }
 
-	}
+            card.value = cardNum;
+            card.cardSuit = (cardData.suit)(int)GD.RandRange(0, 4);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+            cards.Add(card);
+        }
 
+        foreach (cardData c in cards)
+        {
+            GD.Print($"Card: {c.value} of {c.cardSuit}");
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
 
-public struct cardData {
-	public enum suit { Spades, Hearts, Diamonds, Clubs };
-	public int value;
-	public suit cardSuit;
+public struct cardData
+{
+    public enum suit { Spades, Hearts, Diamonds, Clubs };
+    public int value;
+    public suit cardSuit;
 }
