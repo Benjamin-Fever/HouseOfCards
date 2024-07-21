c758328a25f1866b32ae74047569408bc83b3a9e
Benjamin Fever <ben.fever07@gmail.com>
2024-07-21 12:46:09 +1200

    changes

--- Scripts/House.cs
+++ Scripts/House.cs
@@ -1,33 +1,21 @@
 using Godot;
+using Godot.Collections;
 using System;
 
-public partial class House : Node2D
-{
-	[Export] private Sprite2D _house;
-	[Export] private Texture2D _houseT;
-	[Export] public int maxCards = 9;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		setTexture(Main.CardCounter);
+public partial class House : Node2D {
+	[Export] private Array<Sprite2D> _cards;
+
+	private const int MAXSCORE = 20;
+
+	public override void _Ready() {
+		foreach (Sprite2D card in _cards) {
+			card.Visible = false;
+		}
 	}
 
 	public void setTexture(int cardCount){
-		int y = 360;
-		int x = 390;
-		if(cardCount < maxCards){
-			y = 360/maxCards * cardCount;
-			x = 390/maxCards * cardCount;
+		for (int i = 0; i < cardCount; i++) {
+			_cards[i].Visible = true;
 		}
-		AtlasTexture houseTexture = new AtlasTexture(){
-			Atlas = _houseT,
-			Region = new Rect2(0,360 - y,x,y)
-		};
-		_house.Texture = houseTexture;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
 	}
 }
